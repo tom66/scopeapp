255c73ae5c96b6f24667df717584142ff5afdb38
Tom Oldbury <toldbury@gmail.com>
2020-05-22 11:03:20 +0100

    Sync nwaves and initial timebase setup code

--- ZynqScope/ZynqCommands.py
+++ ZynqScope/ZynqCommands.py
@@ -140,6 +140,8 @@ class ZynqCommands(object):
 		"""
 		assert data_mode in ACQ_DATA_MODES
 		
+        print("setup_triggered_acquisition(pre=%d, post=%d, n=%d, mode=0x%02x)", pre_size, post_size, number_acq, data_mode)
+        
 		self.zspi.send_command_ignore_response(zcmd._ACQ_STOP)
 		self.zspi.send_command_ignore_response(zcmd._ACQ_SETUP_TRIGGERED, \
 			_pack_32b_int_arg(pre_size) + \
