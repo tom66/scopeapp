ba4ffb57ff7ab4e48ed3ead22e61dc3bba4db01c
Tom Oldbury <toldbury@gmail.com>
2020-05-22 14:39:13 +0100

    Further debugging

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ print(tb_list)
 
 for idx, item in enumerate(tb_list):
     print("")
-    print("*** Change timebase to %d ***" % idx)
+    print("*** Change timebase to %d with zero pre-time and auto memory depth ***" % idx)
     zsobj.set_next_timebase(idx)
+    zsobj.setup_for_timebase(0)
     print("")
