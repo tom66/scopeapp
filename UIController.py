4129b7d86d9b33f3ceda5180fcf64d48f935b7e3
Tom Oldbury <toldbury@gmail.com>
2020-05-23 12:28:17 +0100

    Debugging

--- UIController.py
+++ UIController.py
@@ -476,7 +476,7 @@ class MainApplication(object):
         Sync instrument parameters.
         """
         print("innerTick")
-        #time.sleep(0.5)
+        time.sleep(0.5)
         #self.ctrl.tick()
     
     def ui_update_clock(self):
