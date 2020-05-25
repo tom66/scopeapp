704ec005c6f04dc324f49f5ceea31ee087ad2855
Tom Oldbury <toldbury@gmail.com>
2020-05-25 14:37:56 +0100

    Hacking

--- ZynqScope/ZynqScopeTask.py
+++ ZynqScope/ZynqScopeTask.py
@@ -40,6 +40,15 @@ class ZynqScopePicklableMemoryBuff(object):
         self.dts = pirawcam_buff.dts
         self.pts = pirawcam_buff.pts
 
+        print("[")
+        print(repr(self.mmal_ptr))
+        print(repr(self.data_ptr)
+        print(repr(self.length))
+        print(repr(self.flags))
+        print(repr(self.dts))
+        print(repr(self.pts))
+        print("]")
+
     def __repr__(self):
         return "<ZynqScopePicklableMemoryBuff mmal_ptr=0x%08x data_ptr=0x%08x length=%d flags=0x%04x dts=%d pts=%d>" % \
             (self.mmal_ptr, self.data_ptr, self.length, self.flags, self.dts, self.pts)
