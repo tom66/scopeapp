37572666b2f1ff8171153fabeb0ef39ddd6b1d3f
Tom Oldbury <toldbury@gmail.com>
2020-05-25 14:39:06 +0100

    Hacking

--- ZynqScope/ZynqScopeTask.py
+++ ZynqScope/ZynqScopeTask.py
@@ -42,7 +42,9 @@ class ZynqScopePicklableMemoryBuff(object):
 
         print("[")
         print(repr(self.mmal_ptr))
+        print(dir(self.mmal_ptr))
         print(repr(self.data_ptr))
+        print(dir(self.data_ptr))
         print(repr(self.length))
         print(repr(self.flags))
         print(repr(self.dts))
