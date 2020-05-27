3928f972aabf7cec48f9e7bf7f233c84713c7b7c
Tom Oldbury <toldbury@gmail.com>
2020-05-27 22:39:25 +0100

    Hacking

--- LoggingHandler.py
+++ LoggingHandler.py
@@ -39,7 +39,7 @@ class BasicFileLogger(logging.StreamHandler):
         ev_time = record.created - self.start_time
         out = ""
 
-        out += "[%8s %8.3f] @%12s ~%16s %s\r\n" % (record.levelname, ev_time, record.threadName, record.module, record.msg % record.args)
+        out += "[%8s %8.3f] %-12s %-16s %s\r\n" % (record.levelname, ev_time, "@" + record.threadName, "~" + record.module, record.msg % record.args)
         self.fp.write(out)
 
     def flush(self):
@@ -70,7 +70,7 @@ class ANSIColouredConsoleLogger(logging.StreamHandler):
         out = ""
 
         out  = "[%s%8s%s " % (code, record.levelname, ANSI_COLOUR_RESET)
-        out += "%8.3f] @%12s ~%16s %s%s%s\r\n" % (ev_time, record.threadName, record.module, code, record.msg % record.args, ANSI_COLOUR_RESET)
+        out += "%8.3f] %-12s %-16s %s%s%s\r\n" % (ev_time, "@" + record.threadName, "~" + record.module, code, record.msg % record.args, ANSI_COLOUR_RESET)
         sys.stdout.write(out)
 
         #if record.levelno >= logging.ERROR:
