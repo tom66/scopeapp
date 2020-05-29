0a393ad7e1f168718d2c9ff367a9660162299ee6
Tom Oldbury <toldbury@gmail.com>
2020-05-29 09:24:20 +0100

    Continued hacking

--- AppConfigManager.py
+++ AppConfigManager.py
@@ -8,6 +8,8 @@ DEFAULT_CONFIG_FILENAME = "main.cfg"
 
 config_defaults = {}
 
+log = logging.getLogger(__name__)
+
 def infer_type(x):
     """For a given value `x' attempt to infer the type.  Return either a float value
     or a string value."""
@@ -50,14 +52,23 @@ class ConfigSubClass(object):
 
 class AppConfigManager(object):
     """
-    This class handles management of the config files.  It supports saving
-    existing config files to disk, and loading and applying new config files.
+    This class handles management of the config files. 
     
     It uses configparser as its core, which is a native Python INI file parser.
+
+    In the future this module will support editing configurations and saving these
+    to disk. 
+
+    Config values can be accessed as dict items or as attributes via the recursive
+    ConfigSubClass.  For instance AppConfigManager.Theme.CSSFile will create subclasses
+    for each attribute that has no children, or will eventually resolve to a float
+    or string.
     """
     def __init__(self, filename=DEFAULT_CONFIG_FILENAME):
         self.c = configparser.ConfigParser(config_defaults, interpolation=configparser.ExtendedInterpolation())
         self.c.read(filename)
+        log.info("Read configuration file: %s" % filename)
+        
         self.subclass = ConfigSubClass(self.c, 0, "root")
     
     def __getitem__(self, key):
--- ScopeApp.py
+++ ScopeApp.py
@@ -21,17 +21,14 @@ lh.set_console_logger(log, logging.INFO)
 class ScopeAppLauncher(object):
     def __init__(self):
         self.cfgmgr = AppConfigManager.AppConfigManager()
-        print(self.cfgmgr)
-        print(self.cfgmgr.Theme.CSSFile)
-        
         self.uiapp = UIController.MainApplication(self.cfgmgr)
 
     def run(self):
         self.uiapp.run()
 
 if __name__ == "__main__":
-    log.warning("Starting YAOS Application on %s" % datetime.now().strftime("%c"))
-    log.warning("Python %s" % sys.version)
+    log.info("Starting YAOS Application on %s" % datetime.now().strftime("%c"))
+    log.info("Python %s" % sys.version)
 
     launcher = ScopeAppLauncher()
     launcher.run()
