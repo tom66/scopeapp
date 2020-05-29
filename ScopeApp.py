0650c8fa49dec48b773f3a1718f8789abf5bd95d
Tom Oldbury <toldbury@gmail.com>
2020-05-29 09:13:28 +0100

    Continued hacking

--- ScopeApp.py
+++ ScopeApp.py
@@ -21,9 +21,10 @@ lh.set_console_logger(log, logging.INFO)
 class ScopeAppLauncher(object):
     def __init__(self):
         self.cfgmgr = AppConfigManager.AppConfigManager()
-        self.uiapp = UIController.MainApplication(self.cfgmgr)
         print(self.cfgmgr)
         print(self.cfgmgr.Theme.CSSFile)
+        
+        self.uiapp = UIController.MainApplication(self.cfgmgr)
 
     def run(self):
         self.uiapp.run()
