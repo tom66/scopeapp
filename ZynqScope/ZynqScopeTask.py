6a5b0ca222d3f1f1b1e3f2fadd3507b3bbde3914
Tom Oldbury <toldbury@gmail.com>
2020-05-29 20:44:01 +0100

    Continued hacking

--- ZynqScope/ZynqScopeTask.py
+++ ZynqScope/ZynqScopeTask.py
@@ -124,7 +124,7 @@ class ZynqScopePicklableMemoryBuff(object):
         fp = open(fn, "w")
         mv = self.get_memoryview()
         print(mv, dir(mv))
-        fp.write(mv.tobytes())
+        fp.write(mv.tobytes().decode("ascii"))
         fp.close()
 
 class ZynqScopeSubprocess(multiprocessing.Process):
