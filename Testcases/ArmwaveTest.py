637c4cdfb9d587d0ed21fe58c9e1ace4c1293b53
Tom Oldbury <toldbury@gmail.com>
2020-06-02 18:39:29 +0100

    Test case changes

--- Testcases/ArmwaveTest.py
+++ Testcases/ArmwaveTest.py
@@ -17,11 +17,24 @@ def main():
     aobj.set_channel_colour(1, (25, 180, 250), 10)
     print("aobj set_target_dimensions")
     aobj.set_target_dimensions(2048, 768)
+
+    print("")
+    print("### First Test Case ###")
+    print("")
+
     print("aobj render_test_to_ppm")
     aobj.render_test_to_ppm("armwave_test.ppm")
+
+    print("")
+    print("### Second Test Case ###")
+    print("")
+
     print("aobj render_test")
     aobj.render_test()
-    print("all done")
+
+    print("")
+    print("### All Done ###")
+    print("")
 
 if __name__ == "__main__":
     main()
