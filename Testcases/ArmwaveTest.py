8d70438eaa5e6857e4f90f9aa8080e0554dab999
Tom Oldbury <toldbury@gmail.com>
2020-06-02 18:16:37 +0100

    Fix bug in armwave test

--- Testcases/ArmwaveTest.py
+++ Testcases/ArmwaveTest.py
@@ -17,6 +17,8 @@ def main():
     aobj.set_channel_colour(1, (25, 180, 250), 10)
     print("aobj set_target_dimensions")
     aobj.set_target_dimensions(2048, 768)
+    print("aobj test_create_am_sine")
+    aobj.test_create_am_sine()
     print("aobj render_test_to_ppm")
     aobj.render_test_to_ppm("armwave_test.ppm")
     print("aobj render_test")
