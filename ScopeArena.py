5bf782aabcfc49d680bae144938bf89181764313
Tom Oldbury <toldbury@gmail.com>
2020-06-04 17:36:24 +0100

    Render hacks

--- ScopeArena.py
+++ ScopeArena.py
@@ -243,9 +243,9 @@ class ScopeArenaController(object):
         # create a Cairo surface which is similar to our window surface for best performance
         # we use get_window() to get the GdkWindow of the GtkWindow, and no, that's not confusing at all.
         self.grat_da.set_size_request(rect.width, rect.height)
-        
+
         alloc = self.grat_da.get_allocated_size()
-        log.info("Cairo arena state (alloc: %d x %d)" % (alloc.width, alloc.height))
+        log.info("Cairo arena state (alloc: %d x %d)" % (alloc.rect.width, alloc.rect.height))
 
         #self.grat_surf = self.window.get_window().create_similar_surface(cairo.Content.COLOR_ALPHA, rect.width, rect.height)
         #self.grat_cr = cairo.Context(self.grat_surf)
