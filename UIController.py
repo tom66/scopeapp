b83fca205a74aa2c70a249203d3b5fe812359786
Tom Oldbury <toldbury@gmail.com>
2020-06-05 13:37:18 +0100

    render debug

--- UIController.py
+++ UIController.py
@@ -50,7 +50,7 @@ popdown_menu_options = [
 ]
 
 # How often to refresh UI data.  Lower number = more CPU, faster refresh.
-UI_REFRESH_MS = 40
+UI_REFRESH_MS = 10
 
 # Minimum delay between refreshes; as load in a tick increases the delay might reduce to keep the UI
 # responsive (& above rate consistent), but we don't want to steal all of GTK's time
