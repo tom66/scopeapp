7a93f7a933bdea7e59549b1e12ddd9a84d98511a
Tom Oldbury <toldbury@gmail.com>
2020-06-06 11:29:02 +0100

    Debugging

--- ZynqScope/ArmwaveRenderEngine.py
+++ ZynqScope/ArmwaveRenderEngine.py
@@ -187,7 +187,7 @@ class ArmwaveRenderEngine(zs.BaseRenderEngine):
         aw.set_wave_pointer_as_testbuf(index % self.test_waveset_count)
 
         #log.info("test_generate")
-        xindex = int((index / 100) % 14)
+        xindex = int((index / 20) % 28)
         log.info("test_generate %d" % (xindex))
         aw.test_generate(xindex)
 
