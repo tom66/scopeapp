ce4c804133fa2b877daab88b6ff66bdfdf60f6ad
Tom Oldbury <toldbury@gmail.com>
2020-06-19 18:04:55 +0100

    Trigger development

--- ZynqScope/ZynqScope.py
+++ ZynqScope/ZynqScope.py
@@ -251,10 +251,12 @@ class ZynqScope(object):
 
         # Connect trigger engine to us.  Start with a default ADC mapping.
         log.debug("ZynqScope connect(): connecting trigger, loading default ADC mapping")
+        print(dir(self.trig_eng))
         self.trig_eng.connect_execute_cb(self.zcmd_execute_cb)
         self.adc_map.set_mapping(-1, +1, 255)
         self.trig_eng.set_adc_mapping(self.adc_map)
         self.trig_eng.set_config(zstrg.ZynqScopeTriggerEdge())
+        print(dir(self.trig_eng))
 
         # Ensure AcqCtrl is reset...
         log.debug("ZynqScope connect(): reset acq_ctrl block on FPGA")
