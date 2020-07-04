00331f7d01d6d4f31236da9a9a752cefb4c36aa2
Tom Oldbury <toldbury@gmail.com>
2020-07-04 17:49:40 +0100

    Move to Xvideo changes

--- ZynqScope/ArmwaveRenderEngine.py
+++ ZynqScope/ArmwaveRenderEngine.py
@@ -108,7 +108,7 @@ class ArmwaveRenderEngine(zs.BaseRenderEngine):
         # Setup armwave
         aw.cleanup()
         aw.setup_render(self.wave_params[0], self.wave_params[1], self.wave_params[2], self.wave_params[3], width, height, \
-            aw.AM_FLAG_GRAT_RENDER_FRAME | aw.AM_FLAG_GRAT_RENDER_DIVS)
+            aw.AM_FLAG_GRAT_RENDER_FRAME | aw.AM_FLAG_GRAT_RENDER_DIVS | aw.AM_FLAG_GRAT_RENDER_SUBDIV)
         aw.test_create_am_sine(0.25, 1e-5, 8)
         log.warning("setup_render done")
 
