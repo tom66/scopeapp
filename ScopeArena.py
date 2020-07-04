ba736ba617168232626f3017f7255f586573d1b2
Tom Oldbury <toldbury@gmail.com>
2020-07-04 14:08:33 +0100

    Move to Xvideo changes

--- ScopeArena.py
+++ ScopeArena.py
@@ -359,9 +359,9 @@ class ScopeArenaController(object):
         return False
 
     def update(self):
-        if not self.first_draw:
-            log.warn("Not done first redraw, skipping update")
-            return
+        #if not self.first_draw:
+        #    log.warn("Not done first redraw, skipping update")
+        #    return
 
         xid = self.img.get_window()
         if xid != None and self.xid != xid.get_xid():
